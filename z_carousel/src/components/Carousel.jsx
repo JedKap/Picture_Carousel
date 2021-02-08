25711d6297ec69ea564f471cf41e5ccdd2a72699
Jed Kaplowitz <Jed.Kaplowitz@gfk.com>
2021-02-08 12:04:58 -0500

    Display Slides

--- z_carousel/src/components/Carousel.jsx
+++ z_carousel/src/components/Carousel.jsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { images } from "../images";
 import Slide from "./Slide";
 
 const Carousel = () => {
+  const [offset, SetOffset] = useState(0);
+
+  const moveCar = (direction) => {
+    switch (direction) {
+      default:
+        break;
+      case "left":
+        if (offset <= -100) SetOffset(offset + 100);
+        break;
+      case "right":
+        if (offset > -100 * (images.length - 1)) SetOffset(offset - 100);
+        break;
+    }
+  };
+
   return (
     <React.Fragment>
       {images && images.length > 0 ? (
         <div style={{ display: "flex", justifyContent: "center" }}>
+          <button id="btnLeft" className="btn" onClick={() => moveCar("left")}>
+            LEFT
+          </button>
           <div className="carousel">
             {images.map((item) => {
               return (
-                <div key={item.id} className="slide">
+                <div
+                  key={item.id}
+                  className="slide"
+                  style={{ transform: `translateX(${offset}%)` }}
+                >
                   <Slide image={item.image} title={item.title} />
                 </div>
               );
             })}
           </div>
+          <button
+            id="btnRight"
+            className="btn"
+            onClick={() => moveCar("right")}
+          >
+            RIGHT
+          </button>
         </div>
       ) : (
         <div>No images to display!</div>
