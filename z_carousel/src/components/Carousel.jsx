e78ae5d9295d88df006ce997ce13853b173eb952
Jed Kaplowitz <Jed.Kaplowitz@gfk.com>
2021-02-08 12:24:05 -0500

    Mouse Drag and Buttons

--- z_carousel/src/components/Carousel.jsx
+++ z_carousel/src/components/Carousel.jsx
@@ -1,37 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { images } from "../images";
 import Slide from "./Slide";
 
 const Carousel = () => {
   const [offset, SetOffset] = useState(0);
+  const [dragging, SetDragging] = useState(false);
+  const [xPos, setXPos] = useState(0);
+  const [dragOff, setDragOff] = useState(0);
+  const carDiv = useRef(null);
 
   const moveCar = (direction) => {
     switch (direction) {
       default:
         break;
       case "left":
-        if (offset <= -100) SetOffset(offset + 100);
+        if (offset <= -1) SetOffset(offset + 1);
         break;
       case "right":
-        if (offset > -100 * (images.length - 1)) SetOffset(offset - 100);
+        if (offset > -1 * (images.length - 1)) SetOffset(offset - 1);
         break;
     }
+  };
+
+  const mouseDown = (e) => {
+    e = e || window.event;
+    carDiv.current.style.cursor = "grab";
+    carDiv.current.style.userSelect = "none";
+    SetDragging(true);
+    setXPos(e.clientX);
+    e.preventDefault();
+  };
+
+  const mouseUp = (e) => {
+    e = e || window.event;
+    carDiv.current.style.cursor = "default";
+    carDiv.current.style.removeProperty("user-select");
+    SetDragging(false);
+    e.preventDefault();
+
+    setDragOff(e.clientX - xPos);
+
+    if (e.clientX - xPos > 1) {
+      moveCar("left");
+    } else if (e.clientX - xPos < -1) {
+      moveCar("right");
+    }
+  };
+
+  const mouseMove = (e) => {
+    if (dragging === true) {
+      if (carDiv) {
+        carDiv.current.style.cursor = "grabbing";
+
+        // THIS ALMOST WORKS
+        // IS SUPPOSED TO MOVE THE SLIDES WITH THE MOUSE
+        // setDragOff(e.clientX - xPos);
+        // carDiv.current.scrollLeft = xPos - dragOff;
+      }
+    }
+    e.preventDefault();
   };
 
   return (
     <React.Fragment>
       {images && images.length > 0 ? (
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className="outerDiv">
           <button id="btnLeft" className="btn" onClick={() => moveCar("left")}>
-            LEFT
+            <div className="glyph">&#9664;</div>
           </button>
-          <div className="carousel">
+          <div
+            className="carousel"
+            onMouseDown={(e) => mouseDown(e)}
+            onMouseUp={(e) => mouseUp(e)}
+            onMouseMove={(e) => mouseMove(e)}
+            ref={carDiv}
+          >
             {images.map((item) => {
               return (
                 <div
                   key={item.id}
                   className="slide"
-                  style={{ transform: `translateX(${offset}%)` }}
+                  style={{ transform: `translateX(${offset * 100}%)` }}
                 >
                   <Slide image={item.image} title={item.title} />
                 </div>
@@ -43,7 +92,7 @@ const Carousel = () => {
             className="btn"
             onClick={() => moveCar("right")}
           >
-            RIGHT
+            <div className="glyph">&#9654;</div>
           </button>
         </div>
       ) : (
